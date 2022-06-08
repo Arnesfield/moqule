604e89b4ae789a71f75079b28d82475a8f2d5db6
Jefferson Rylee <rylee.jeff385@gmail.com>
2022-06-08 11:15:02 +0800

    Update rollup config Skip sourcemap and umd bundle unless production Use unpkg instead of browser field in package.json Use minified umd bundle for package.json

--- rollup.config.js
+++ rollup.config.js
@@ -7,45 +7,46 @@ import pkg from './package.json';
 const name = pkg.name.slice(pkg.name.lastIndexOf('/') + 1);
 const input = 'src/index.ts';
 const inputUmd = 'src/index.umd.ts';
-const plugins = [esbuild()];
+// skip sourcemap and umd unless production
+const PROD = !process.env.ROLLUP_WATCH;
 
-function umd(options) {
-  return {
-    file: pkg.browser,
-    format: 'umd',
-    name,
-    sourcemap: true,
-    exports: 'default',
-    ...options
-  };
+function out(options) {
+  return { sourcemap: PROD, exports: 'named', ...options };
 }
 
-export default args => [
+function umd(options) {
+  return out({ format: 'umd', name, exports: 'default', ...options });
+}
+
+const configs = [
   {
     input,
     output: [
-      { file: pkg.main, format: 'cjs', sourcemap: true, exports: 'named' },
-      { file: pkg.module, format: 'esm', sourcemap: true, exports: 'named' }
+      out({ file: pkg.main, format: 'cjs' }),
+      out({ file: pkg.module, format: 'esm' })
     ],
-    plugins
-  },
-  {
-    input: inputUmd,
-    output: [
-      umd(),
-      umd({
-        file: pkg.browser.replace(/\.js$/, '.min.js'),
-        plugins: [terser()]
-      })
-    ],
-    plugins
+    plugins: [esbuild()]
   },
   { input, output: { file: pkg.types, format: 'esm' }, plugins: [dts()] },
   // type checking only
   {
     input,
-    output: !args.watch ? { file: '/dev/null' } : undefined,
+    output: PROD ? { file: '/dev/null' } : undefined,
     plugins: [typescript({ noEmit: true, sourceMap: false })],
     watch: { skipWrite: true }
   }
 ];
+
+if (PROD) {
+  const bundle = {
+    input: inputUmd,
+    output: [
+      umd({ file: pkg.unpkg.replace(/\.min\.js$/, '.js') }),
+      umd({ file: pkg.unpkg, plugins: [terser()] })
+    ],
+    plugins: [esbuild()]
+  };
+  configs.splice(1, 0, bundle);
+}
+
+export default configs;
