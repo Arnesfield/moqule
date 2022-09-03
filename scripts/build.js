9650a4302f6c5c32db56dcb47f29c2e5a45c8154
Jefferson Rylee <rylee.jeff385@gmail.com>
2022-09-03 14:50:19 +0800

    Add lint:strict script Update build script to use check

--- scripts/build.js
+++ scripts/build.js
@@ -5,8 +5,6 @@ const WATCH = args.includes('-w');
 const PROD = !WATCH || args.includes('-p');
 
 const esbuildOpts = [WATCH && '--watch', PROD && '--sourcemap'];
-const lint = !WATCH ? ['lint', '--max-warnings 0'] : [];
-const ts = !WATCH ? ['ts'] : [];
 const cjs = ['build:cjs', ...esbuildOpts];
 const esm = ['build:esm', WATCH && '--log-level=silent', ...esbuildOpts];
 const rollup = [
@@ -15,13 +13,12 @@ const rollup = [
   PROD && '--environment NODE_ENV:production'
 ];
 
-const commands = [lint, ts, cjs, esm, rollup]
-  .map(c => {
-    c = c.filter(arg => arg);
-    c = c.length > 1 ? c.slice(0, 1).concat('--', c.slice(1)) : c;
-    return c.join(' ');
-  })
-  .filter(c => c);
+const commands = [cjs, esm, rollup].map(c => {
+  c = c.filter(arg => arg);
+  c = c.length > 1 ? c.slice(0, 1).concat('--', c.slice(1)) : c;
+  return c.join(' ');
+});
+!WATCH && commands.unshift('check');
 
 runAll(commands, {
   parallel: true,
