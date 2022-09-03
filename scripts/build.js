8304c310c538dae71c87935a1be8634f79f23cbb
Jefferson Rylee <rylee.jeff385@gmail.com>
2022-09-03 10:19:08 +0800

    Update build script to use npm-run-all

--- scripts/build.js
+++ scripts/build.js
@@ -1,41 +1,31 @@
-import { exec } from './exec.js';
+import runAll from 'npm-run-all';
 
 const args = process.argv.slice(2);
 const WATCH = args.includes('-w');
 const PROD = !WATCH || args.includes('-p');
 
-const input = 'src/index.ts';
-const esbuildOpts = [
-  '--bundle',
-  '--outdir=lib',
-  WATCH && '--watch',
-  PROD && '--sourcemap'
+const esbuildOpts = [WATCH && '--watch', PROD && '--sourcemap'];
+const lint = !WATCH ? ['lint', '--max-warnings 0'] : [];
+const ts = !WATCH ? ['ts'] : [];
+const cjs = ['build:cjs', ...esbuildOpts];
+const esm = ['build:esm', WATCH && '--log-level=silent', ...esbuildOpts];
+const rollup = [
+  'build:rollup',
+  WATCH && '--watch --no-watch.clearScreen',
+  PROD && '--environment NODE_ENV:production'
 ];
 
-const checks = !WATCH ? ['npm run check'.split(' ')] : [];
-exec(
-  ...checks,
-  [
-    'esbuild',
-    input,
-    '--format=cjs',
-    '--out-extension:.js=.cjs',
-    ...esbuildOpts
-  ],
-  [
-    'esbuild',
-    input,
-    '--format=esm',
-    '--out-extension:.js=.mjs',
-    WATCH && '--log-level=silent',
-    ...esbuildOpts
-  ],
-  [
-    'rollup',
-    '-c',
-    WATCH && '--watch',
-    WATCH && '--no-watch.clearScreen',
-    PROD && '--environment',
-    PROD && 'NODE_ENV:production'
-  ]
-);
+const commands = [lint, ts, cjs, esm, rollup]
+  .map(c => {
+    c = c.filter(arg => arg);
+    c = c.length > 1 ? c.slice(0, 1).concat('--', c.slice(1)) : c;
+    return c.join(' ');
+  })
+  .filter(c => c);
+
+runAll(commands, {
+  parallel: true,
+  stdin: process.stdin,
+  stdout: process.stdout,
+  stderr: process.stderr
+}).catch(() => {});
