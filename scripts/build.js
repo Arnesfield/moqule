8796da63d036fdcb1d34967010113290c9a32707
Jefferson Rylee <rylee.jeff385@gmail.com>
2022-09-03 19:38:34 +0800

    Simplify build script

--- scripts/build.js
+++ scripts/build.js
@@ -4,23 +4,36 @@ const args = process.argv.slice(2);
 const WATCH = args.includes('-w');
 const PROD = !WATCH || args.includes('-p');
 
-const esbuildOpts = [WATCH && '--watch', PROD && '--sourcemap'];
-const cjs = ['build:cjs', ...esbuildOpts];
-const esm = ['build:esm', WATCH && '--log-level=silent', ...esbuildOpts];
+const esbuildOpts = [
+  '--bundle --outdir=lib',
+  WATCH && '--watch',
+  PROD && '--sourcemap'
+];
+const cjs = [
+  'build:js',
+  '--format=cjs --out-extension:.js=.cjs',
+  ...esbuildOpts
+];
+const esm = [
+  'build:js',
+  '--format=esm --out-extension:.js=.mjs',
+  WATCH && '--log-level=silent',
+  ...esbuildOpts
+];
 const rollup = [
   'build:rollup',
   WATCH && '--watch --no-watch.clearScreen',
   PROD && '--environment NODE_ENV:production'
 ];
 
-const commands = [cjs, esm, rollup].map(c => {
-  c = c.filter(arg => arg);
-  c = c.length > 1 ? c.slice(0, 1).concat('--', c.slice(1)) : c;
-  return c.join(' ');
+const scripts = [cjs, esm, rollup].map(s => {
+  s = s.filter(arg => arg);
+  s = s.length > 1 ? s.slice(0, 1).concat('--', s.slice(1)) : s;
+  return s.join(' ');
 });
-!WATCH && commands.unshift('check');
+!WATCH && scripts.unshift('check');
 
-runAll(commands, {
+runAll(scripts, {
   parallel: true,
   stdin: process.stdin,
   stdout: process.stdout,
